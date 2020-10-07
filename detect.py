fff62318fc00b062609d6b390a7c8c7d4df9ee77
KASSEM Hachi <30910460+KassemHachi@users.noreply.github.com>
2020-10-07 15:58:34 +0100

    Update detect.py

--- detect.py
+++ detect.py
@@ -2,7 +2,7 @@ import cv2
 
 # Load the cascade
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-img = cv2.imread('img2.jpg')
+img = cv2.imread('input.jpg')
 
 # Convert into grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
